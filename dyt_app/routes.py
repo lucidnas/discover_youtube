d7379e30ef1efda76091e114f7187e280d8c2fcd
Abdulnasir Mohammed <abdulnasir.dev@gmail.com>
2018-03-03 23:55:54 -0500

    new changes

--- dyt_app/routes.py
+++ dyt_app/routes.py
@@ -1,17 +1,45 @@
 from dyt_app import app
 from flask import render_template
 import logic
+import movie_properties
 
 
-list_of_movies = logic.movie_tiles_info()
-print list_of_movies
+#list_of_movies = logic.movie_tiles_info()
+#print list_of_movies
+
+movies_list = {"spiderman" : {
+    'poster': u'https://image.tmdb.org/t/p/w500/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg',
+    'trailer': u'rk-dF1lIbIg',
+    'title': u'Spider-Man: Homecoming'
+  },
+  "despicableme3" : {
+    'poster': u'https://image.tmdb.org/t/p/w500/5qcUGqWoWhEsoQwNUrtf3y3fcWn.jpg',
+    'trailer': u'euz-KBBfAAo',
+    'title': u'Despicable Me 3'
+  },
+  "annabelle" : {
+    'poster': u'https://image.tmdb.org/t/p/w500/tb86j8jVCVsdZnzf8I6cIi65IeM.jpg',
+    'trailer': u'KisPhy7T__Q',
+    'title': u'Annabelle: Creation'
+  }
+}
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Discover Youtube', movies=list_of_movies)
+    return render_template('index.html', title='Discover Youtube', movies=movies_list)
 
-@app.route('/movie/<movie_title>')
-def movie_page(movie_title):
-    return render_template('movie_page.html', movie_title=movie_title, movie_poster='', movie_trailer='')
+@app.route('/movie/<movie>')
+def movie_page(movie):
+    return render_template('movie_page.html', movie=movies_list[movie])
+
+@app.route('/search/<input>')
+def search_page(input):
+    pass
+
+def about_page():
+    pass
+
+def contact_page():
+    pass
 
