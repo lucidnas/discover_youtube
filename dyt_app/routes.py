76fa1b4b1f1c841fceb54a308507d79b8c501a51
Abdulnasir Mohammed <abdulnasir.me@gmail.com>
2018-04-11 21:42:42 -0400

    update

--- dyt_app/routes.py
+++ dyt_app/routes.py
@@ -5,21 +5,21 @@ from flask import render_template
 #print list_of_movies
 
 movies_list = {"spiderman" : {
-    'poster': u'https://image.tmdb.org/t/p/w500/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg',
-    'trailer': u'rk-dF1lIbIg',
-    'title': u'Spider-Man: Homecoming',
-    'ratings': u'98%',
-    'cast': u'stars'
+    'poster': 'https://image.tmdb.org/t/p/w500/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg',
+    'trailer': 'rk-dF1lIbIg',
+    'title': 'Spider-Man: Homecoming',
+    'ratings': '98%',
+    'cast': 'stars'
   }, 
   "despicableme3" : {
-    'poster': u'https://image.tmdb.org/t/p/w500/5qcUGqWoWhEsoQwNUrtf3y3fcWn.jpg',
-    'trailer': u'xM0ODz7v0Rw',
-    'title': u'Despicable Me 3'
+    'poster': 'https://image.tmdb.org/t/p/w500/5qcUGqWoWhEsoQwNUrtf3y3fcWn.jpg',
+    'trailer': 'xM0ODz7v0Rw',
+    'title': 'Despicable Me 3'
   },
   "annabelle" : {
-    'poster': u'https://image.tmdb.org/t/p/w500/tb86j8jVCVsdZnzf8I6cIi65IeM.jpg',
-    'trailer': u'KisPhy7T__Q',
-    'title': u'Annabelle: Creation'
+    'poster': 'https://image.tmdb.org/t/p/w500/tb86j8jVCVsdZnzf8I6cIi65IeM.jpg',
+    'trailer': 'KisPhy7T__Q',
+    'title': 'Annabelle: Creation'
   }
 }
 
